4590f0dacbcb99854d592c094063d39ec3272f8c
chen <3290670841@qq.com>
2024-01-11 10:13:09 +0800

    2024.1.11

--- Code/sum.c
+++ Code/sum.c
@@ -1,10 +1,23 @@
-#include "sum.h"
+#include "stdio.h"
+#include "windows.h"
 
-int nums=1;
+int nums=2;
 int sum(int a,int b)
 {
-    int sum;
+    int sum = 0;
     sum=a+b;
     sum+=nums;
     return sum;
+}
+
+int main()
+{
+    int a = 1,b = 2;
+    int num;
+
+    num = sum(a,b);
+    printf("%d\n",num);
+
+    system("pause");
+    return 0;
 }
